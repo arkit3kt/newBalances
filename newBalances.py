d65a91ad4692f95b0e18af5f72e10a091da4d3a2
the arkitekt <exodust@protonmail.com>
2018-10-31 21:34:43 -0400

    add total addresses with balance > 0

--- newBalances.py
+++ newBalances.py
@@ -8,23 +8,26 @@ newbalance = 0
 with open('C:\posqdbSnapshot1\Addresses.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-        try:
-            accounts.append([{'address':row[1], 'balance':row[2]}])
-        except:
-            accounts.append([{'address':row[1], 'balance':row[2]}])
+        accounts.append([{'address':row[1], 'balance':row[2]}])
+
 # Determine new Balance + Circulation
+balancecount = 0
 for i in range(len(accounts) - 1):
     balance = accounts[count][0]['balance']
     address = accounts[count][0]['address']
     address = str(address)
     balance = float(balance)
-    print("\nAddress = ", address)
-    print('Old Balance = ', balance * OldCoin)
-    print('New Balance = ', balance * NewCoin)
-    newbalance += balance
-
+    if balance > 0:
+        print("\n#", count, " Address = ", address)
+        print('Old Balance = ', balance * OldCoin)
+        print('New Balance = ', balance * NewCoin)
+        newbalance += balance
+        balancecount += 1
     count += 1
-print('\nTotal POSQ in circulation', newbalance  * OldCoin)
+
+print("\nTotal Addresses", count-1)
+print("Total addresses with balance > 0 :", balancecount)
+print('Total POSQ in circulation', newbalance * OldCoin)
 print('NEW Total circulation : ', newbalance * NewCoin)
 
 
